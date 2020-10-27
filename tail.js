d1545957a471694f08fe3fe77182ab27ee56393f
deonleer <deon.leer96@gmail.com>
2020-10-27 05:12:59 +0000

    added tail function

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  let pass = "";
+  let logical = "";
+  if (actual === expected) {
+    pass = "Passed";
+    logical = "===";
+  } else if (actual !== expected) {
+    pass = "Failed";
+    logical = "==!";
+  }
+  console.log(`Assertion ${pass}: [actual] ${logical} [expected]`);
+};
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray[i - 1] = array[i];
+  }
+  return newArray;
+};
