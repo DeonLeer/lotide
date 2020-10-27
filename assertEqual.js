7f5ecce397ac449f2dbbcb1e036e7ec5fd3feb94
deonleer <deon.leer96@gmail.com>
2020-10-27 04:49:24 +0000

    🤬️🤬️🤬️🤬️

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("👍️👍️👍️👍️ Assertion Passed: [actual] === [expected]");
+  } else if (actual !== expected) {
+    console.log("🤬️🤬️🤬️🤬️ Assertion Failed: [actual] ==! [expected]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
