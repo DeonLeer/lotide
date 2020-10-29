8eece5ff218a8ee1e83f24149727af02db9e0dda
deonleer <deon.leer96@gmail.com>
2020-10-29 22:04:14 +0000

    linting

--- eqObjects.js
+++ eqObjects.js
@@ -25,24 +25,22 @@ const eqArrays = (arr1, arr2) => {
 };
 
 const eqObjects = (obj1, obj2) => {
-  obj1Keys = Object.keys(obj1);
-  obj2Keys = Object.keys(obj2);
+  let obj1Keys = Object.keys(obj1);
+  let obj2Keys = Object.keys(obj2);
   if (obj1Keys.length !== obj2Keys.length) {
-    return false
+    return false;
   } else {
-    counter = 0;
+    let counter = 0;
     for (let key of obj1Keys) {
-      if (key !== obj2Keys[counter]) {
+      if (Array.isArray(obj1[key]) !== Array.isArray(obj2[key])) {
         return false;
-      } else if (Array.isArray(obj1[key]) !== Array.isArray(obj2[key])){
-        return false;
-      } else if (Array.isArray(obj1[key]) === true ){
-        if (eqArrays(obj1[key], obj2[key]) === false){
-          return false
+      } else if (Array.isArray(obj1[key]) === true) {
+        if (eqArrays(obj1[key], obj2[key]) === false) {
+          return false;
         }
       } else if ((obj1[key]) !== obj2[key]) {
         return false;
-      } counter += 1
+      } counter += 1;
     }
   } return true;
 };
@@ -53,6 +51,6 @@ const abc = { a: "1", b: "2", c: "3" };
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-const cd2 = { c: "1", d: ["2", 3, 4]};
-assertEqual(eqObjects(cd, cd2), true); 
+const cd2 = { c: "1", d: ["2", 3]};
+assertEqual(eqObjects(cd, cd2), true);
 
