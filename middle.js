ebc3b3936ec8c0e5143087f89da2d6400561d659
deonleer <deon.leer96@gmail.com>
2020-11-03 16:06:31 +0000

    added test file and refactored

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-const eqArrays = (arr1, arr2) => {
- 
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  let pass = "";
-  let logical = "";
-  if (eqArrays(arr1, arr2)) {
-    pass = "Passed";
-    logical = "===";
-  } else if (!eqArrays(arr1, arr2)) {
-    pass = "Failed";
-    logical = "!==";
-  }
-  console.log(`Assertion ${pass}: [array 1] ${logical} [array 2]`);
-};
-
 const middle = (arr) => {
   let midd = [];
   if (arr.length === 0 || arr.length === 1) {
@@ -36,3 +10,5 @@ const middle = (arr) => {
     return midd;
   }
 };
+
+module.exports = middle;
